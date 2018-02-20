967e2acaad921afe6f5df20beebab583d605f19b
JaeHong <studyexe1@naver.com>
2018-02-20 13:20:30 +0900

    12345

--- src/com/hong/s1/Main.java
+++ src/com/hong/s1/Main.java
@@ -1,15 +1,34 @@
 package com.hong.s1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Program Start");
-		
-		SchoolController school = new SchoolController();
-		school.start();
-		
-		
 
+		System.out.println("program Start");
+		SchoolController school = new SchoolController();
+		school.start();		//new SchoolController().start();  로 두줄을 합칠수 있다.  
+							// school이 주소값  new SchoolController()도 주소값이므로 가능. 주소값에 있는 start메서드 실행
+		
+		//CF////////
+		//객체 : 눈에 보이는 사물 또는 개념
+		//학생 : 3명  , 1명당 object
+		/*Scanner sc = new Scanner(System.in);
+		Student [] ar = new Student[2];
+		Student student = new Student();
+		for(int i =0;i<ar.length;i++) {
+			student.name = sc.next();
+			ar[i]=student;
+			System.out.println(ar[i].name);
+		}
+		///// 위 아래는 큰 차이가 잇음 객체 갯수의 차이 
+		for(int i =0;i<ar.length;i++) {
+			student = new Student();
+			student.name = sc.next();
+			ar[i]=student;
+			System.out.println(ar[i].name);
+		}*/
 	}
 
 }
--- src/com/hong/s1/SchoolController.java
+++ src/com/hong/s1/SchoolController.java
@@ -3,65 +3,55 @@ package com.hong.s1;
 import java.util.Scanner;
 
 public class SchoolController {
+	//메서드 명은  start 
+	//return  x
+	//내용 
+	//1 . 학생 등록(추가)
+	//2 . 성적 입력
+	//3 . 성적 조회
+	//4 . 전체 조회
+	//5 . 프로그램 종료
 
-	//메서드 명 start
-	//리턴은 없음 
-	//내용은 
-	//1. 학생등록
-	//2. 성적입력
-	//3. 성적조회
-	//4. 전체조회
-	//5. 프로그램 종료
-	Student[] student1=null;
-			
 	public void start() {
+		int select = 0;
+		boolean check = true;
+		Student [] student1= null;
 		Scanner sc= new Scanner(System.in);
-		boolean check=true;
-		
+		StudentService ss= new StudentService();
+		StudentView sv = new StudentView();
+
 		while(check) {
-			System.out.println("메뉴를 선택해 주세요.");
-			System.out.println("1. 학생등록 | 2. 성적 입력 | 3. 성적조회 | 4. 전체조회 | 5. 프로그램 종료");
-			int num =sc.nextInt();
-			
-			switch(num) {
+			System.out.println("번호를 입력해주세요");
+			System.out.println("1.학생 등록 |2. 성정 입력 |3. 성적 조회 |4. 전체 조회 |5.프로그램 종료");
+			select = sc.nextInt();
+			switch(select) {
+
 			case 1:
-				System.out.println("학생을 등록 해주세요.");
-				StudentService ss= new StudentService();
+
 				student1=ss.addStudent();
-				
-				
 				break;
-				
 			case 2:
-				System.out.println("성적을 입력해주세요.");
-				break;
+
+				System.out.println("2 . 성적 입력");
+
 			case 3:
-				System.out.println("성적을 조회 합니다.");
-				break;
+
+				System.out.println("3 . 성적 조회");
 			case 4:
-				System.out.println("전체 내용을 조회 합니다.");
-				StudentView sv=new StudentView();
+
+				System.out.println("4 . 전체 조회");
 				sv.view(student1);
-				
-				break;
+
 			case 5:
-				System.out.println("프로그램을 종료 합니다.");
-				check =false;
-				break;
-			default : 
-				System.out.println("번호를 잘못 입력했습니다. 다시 선택 해주세요.");
+
+				System.out.println("5 . 프로그램 종료");
+				check = false;
+			default :
+				System.out.println("없는 번호입니다.");
 				continue;
-		
-			
-			
 			}//switch
-			
-			
-			
+
 		}//while
-		
-	}//start
-	
-	
-	
+
+	}
 }
