9cebabd39bba14b6ffb6102e9aeadce93b3d9669
JaeHong <studyexe1@naver.com>
2018-02-20 10:23:44 +0900

    test1

--- src/com/hong/s1/Main.java
+++ src/com/hong/s1/Main.java
@@ -3,6 +3,8 @@ package com.hong.s1;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Program Start");
+		
 		SchoolController school = new SchoolController();
 		school.start();
 		
